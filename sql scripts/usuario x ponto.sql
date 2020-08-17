f864b2bc5ca731c56c44d776f5f17c26651ecbef
Marcus Vinicius <marcuspestana@ic.ufal.br>
2020-08-17 16:58:13 -0300

    Update usuario x ponto.sql

--- sql scripts/usuario x ponto.sql
+++ sql scripts/usuario x ponto.sql
@@ -1,7 +1,8 @@
-SELECT u.id, u.nome, u.email, u.cod_ibge_id,
-		ponto.id, ponto.nome
-	FROM public.relatorios_usuario as u, public.relatorios_ponto as ponto, 
-	public.relatorios_ponto_membro as pm
-	where u.id = pm.usuario_id and ponto.id = pm.ponto_id;
+SELECT u.id, u.nome, u.email,
+		ponto.id, ponto.nome, ponto.cod_ibge_id, loc.municipio
+	FROM public.relatorios_usuario as u, public.relatorios_ponto as ponto,
+	public.relatorios_ponto_membro as pm, public.relatorios_localizacao as loc
+	where u.id = pm.usuario_id and ponto.id = pm.ponto_id
+	and ponto.cod_ibge_id = loc.cod_ibge;
 	
 	
